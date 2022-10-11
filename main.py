61d4842a115a8f1e17ce41563dedad030d2c6ebf
Kranidos1 <76439725+Kranidos1@users.noreply.github.com>
2022-10-11 18:59:54 +0200

    final push

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ import os
 app = wx.App()
 mainFrame = MainFrame()
 mainFrame.Show()
-#diciamo che il max e' 27
-#fontsInfo = [(('Bauhaus 93', 'BAUHS93.TTF'), (255, 0, 0, 255), 23) ,(('Times New Roman', 'TIMES.TTF'), (0, 0, 0, 255), 14) ,(('Times New Roman', 'TIMES.TTF'),
-                              #                                               (0, 0, 0, 255), 22) ,(('Times New Roman', 'TIMES.TTF'), (0, 0, 0, 255), 28)]
-#save_name = os.path.join(os.path.expanduser("~"), "Desktop/", "test1.pdf")
-#pdf = PDF(save_name)
-#pdf.__headerFattura__(["Qualcosa fdsfdsfdsfdsTitolo" ,"Via Rossfdsfdsfdsfdsi 100,Vollfddsa,Napoli,80040","Num Cell 339820976 PIVA 848484","qualcosa.ifdfdsfsdfdssfdsfdsfdst - qualcosa@gmail.com"] ,"test1.jpg" ,"s" ,fontsInfo)
-#info = (('Bauhaus 93', 'BAUHS93.TTF'), (255, 0, 0, 255), 23)
-#personal = PersonalizedDialog(info)
-#personal.ShowModal()
 app.MainLoop()
 
 
